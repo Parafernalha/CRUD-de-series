5136ba34013065bbf5843e48516f242d365101d1
Douglas de Oliveira Fernandes <douglas.oliveiras2210@gmail.com>
2021-11-21 22:39:07 -0300

    CRUD de series

---
+++ CRUD de series/Classes/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DIO.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
